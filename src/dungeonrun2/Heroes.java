ba37909ee181062f4bb4fbcf70f6b5e1f5ba5b00
fabia <fabia@Asteria.lan>
2020-11-12 09:36:48 +0100

    Tog bort "Abstract" från classnamnet

---
+++ src/dungeonrun2/Heroes.java
@@ -0,0 +1,53 @@
+package dungeonrun2;
+
+public abstract class Heroes {
+    
+    private int initiative;
+    private int health;
+    private int attack;
+    private int agility;
+    private String name;
+
+    public Heroes(int initiative, int health, int attack, int agility, String name) {
+        this.initiative = initiative;
+        this.health = health;
+        this.attack = attack;
+        this.agility = agility;
+        this.name = name;
+    }
+
+    public int getInitiative() {
+        return initiative;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    
+    
+    public static void stats() {
+        System.out.println("Nu blev det fel");
+    }
+
+    @Override
+    public String toString() {
+        return "Hero: " + name;
+    }
+}
